00ff4b6354dfc7aaed80841b663ac8e1bf0bad40
greatxue <131960871+greatxue@users.noreply.github.com>
2023-05-22 11:20:25 +0800

    update NaiveLetterFreqGuesser 5/22

--- ucb_CS61B/proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
+++ ucb_CS61B/proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -1,8 +1,11 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class NaiveLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -21,14 +24,45 @@ public class NaiveLetterFreqGuesser implements Guesser {
      *  This task is similar to something you did in hw0b! */
     public Map<Character, Integer> getFrequencyMap() {
         // TODO: Fill in this method.
-        return null;
+        Map<Character, Integer> frequencyMap = new HashMap();
+        for (char c = 'a'; c <= 'z'; c++) {
+            int num = 0;
+            for (String word : words) {
+                for (int i = 0; i < word.length(); i++) {
+                    char letter = word.charAt(i);
+                    if (letter == c) {
+                        num++;
+                        frequencyMap.put(c, num);
+                    }
+                }
+            }
+        }
+        return frequencyMap;
     }
 
     /** Returns the most common letter in WORDS that has not yet been guessed
      *  (and therefore isn't present in GUESSES). */
     public char getGuess(List<Character> guesses) {
         // TODO: Fill in this method.
-        return '?';
+        Map<Character, Integer> frequencyMap = getFrequencyMap();
+        if (frequencyMap.isEmpty()){
+            return '?';
+        }
+        TreeMap<Character, Integer> sortedMap = new TreeMap<>(frequencyMap);
+        for (Character ch: guesses){
+            sortedMap.remove(ch);
+        }
+        int maxFrequency = 0;
+        char getguess = '?';
+        for (Map.Entry<Character, Integer> entry : sortedMap.entrySet()){
+            char c = entry.getKey();
+            int frequency = entry.getValue();
+            if (frequency > maxFrequency){
+                maxFrequency = frequency;
+                getguess = c;
+            }
+        }
+        return getguess;
     }
 
     public static void main(String[] args) {
